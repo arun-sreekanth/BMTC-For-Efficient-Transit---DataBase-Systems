8a916070f50ace0aacce08ccb11215fa12b987e4
arun-sreekanth <141525982+arun-sreekanth@users.noreply.github.com>
2023-08-05 19:10:10 -0500

    Add files via upload

---
+++ Scripts/bus_type.sql
@@ -0,0 +1,3 @@
+/*Insert scripts for tabel Bustype*/
+INSERT INTO Spring23_S003_15_Bustype (bustype, general_seats, female_seats, sr_citizen_seats, phy_handicapped_seats) VALUES ('Regular', 25, 12, 4, 4);
+INSERT INTO Spring23_S003_15_Bustype (bustype, general_seats, female_seats, sr_citizen_seats, phy_handicapped_seats) VALUES ('Vjara', 21, 0, 2, 2);
